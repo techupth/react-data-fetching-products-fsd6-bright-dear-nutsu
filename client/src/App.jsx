9dab28827733543c459b6ecef63b7fe1736fb673
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-07 16:23:17 +0700

    feat: add productList state and data fetching & deleting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,81 @@
-import "./App.css";
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+//////////// State
+// 1. มี 1 states : productList
+// 2. มี 1 event : onClick ที่ button มี callback คือ handleDelete
+// 3. ประกาศ functions : handleDelete
+
+//////////// Data Fetching and Deleting
+// 1.API :
+//    1.1 GET: http://localhost:4001/products
+//    1.2 DELETE: http://localhost:4001/products/:id
+// 2.axios : ติดตั้ง, import and execute
+//    2.1 สร้าง request ใน function แล้วอัพเดท state
+//    2.2 execute ใน useEffect เมื่อ โหลดหน้าเวปครั้งแรกครั้งเดียว
+// 3.นำข้อมูลจาก Response มา Render
 
 function App() {
+  const [productList, setProductList] = useState([]);
+
+  const getAllProduct = async () => {
+    try {
+      const result = await axios.get('http://localhost:4001/products');
+      console.log(result.data.data);
+      setProductList(result.data.data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllProduct();
+  }, []);
+
+  const handleDelete = async (deleteId) => {
+    try {
+      await axios.delete(`http://localhost:4001/products/${deleteId}`);
+      // console.log(resultDelete);
+      const newProductList = productList.filter((item) => item.id !== deleteId);
+      setProductList(newProductList);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="app-wrapper">
         <h1 className="app-title">Products</h1>
       </div>
       <div className="product-list">
-        <div className="product">
-          <div className="product-preview">
-            <img
-              src="https://via.placeholder.com/350/350"
-              alt="some product"
-              width="350"
-              height="350"
-            />
-          </div>
-          <div className="product-detail">
-            <h1>Product name: ...</h1>
-            <h2>Product price: ... Baht</h2>
-            <p>Product description: .....</p>
-          </div>
+        {productList.map((item, index) => {
+          return (
+            <div className="product" key={index}>
+              <div className="product-preview">
+                <img
+                  src={item.image}
+                  alt="some product"
+                  width="350"
+                  height="350"
+                />
+              </div>
+              <div className="product-detail">
+                <h1>Product name: {item.name}</h1>
+                <h2>Product price: {item.price}Baht</h2>
+                <p>Product description: {item.description}</p>
+              </div>
 
-          <button className="delete-button">x</button>
-        </div>
+              <button
+                className="delete-button"
+                onClick={() => handleDelete(item.id)}
+              >
+                x
+              </button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
