04b4097bf8207661a64f56090d4999455b4ffb06
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-08 20:22:45 +0700

    feat: add isLoading and serverError states with conditional rendering

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import axios from 'axios';
 
 //////////// State
-// 1. มี 1 states : productList
+// 1. มี 1 states : productList = []
 // 2. มี 1 event : onClick ที่ button มี callback คือ handleDelete
 // 3. ประกาศ functions : handleDelete
 
@@ -16,16 +16,34 @@ import axios from 'axios';
 //    2.2 execute ใน useEffect เมื่อ โหลดหน้าเวปครั้งแรกครั้งเดียว
 // 3.นำข้อมูลจาก Response มา Render
 
+/////////// Challenge-Must try!
+// 1. เพิ่ม 2 states : isLoading = false กับ serverError = null
+// 2. ใน request function อัพเดท state ทั้ง 2
+// 3. นำค่า ของทั้ง 2 states มาสร้างเงื่อนไขในการ render ข้อความ
+//    "Loading..." กับ "Fetching Error..."
+
 function App() {
   const [productList, setProductList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [serverError, setServerError] = useState(null);
 
   const getAllProduct = async () => {
     try {
+      ///// เริ่ม Load ให้ isLoading เป็น true
+      setIsLoading(true);
+      ///// reset serverError ก่อนการ fetch ข้อมูลใหม่
+      setServerError(null);
       const result = await axios.get('http://localhost:4001/products');
-      console.log(result.data.data);
-      setProductList(result.data.data);
+      // console.log(result.data.data);
+      setProductList(result?.data?.data);
+      ///// Load เสร็จ ให้ isLoading เป็น false
+      setIsLoading(false);
     } catch (error) {
-      alert(error);
+      // console.log(error);
+      ///// เจอ Error ให้อัพเดท error ไปใน state
+      setIsLoading(false);
+      setServerError(error);
+      console.log(serverError);
     }
   };
 
@@ -40,45 +58,59 @@ function App() {
       const newProductList = productList.filter((item) => item.id !== deleteId);
       setProductList(newProductList);
     } catch (error) {
-      alert(error);
+      console.logt(error);
     }
   };
 
-  return (
-    <div className="App">
-      <div className="app-wrapper">
-        <h1 className="app-title">Products</h1>
-      </div>
-      <div className="product-list">
-        {productList.map((item, index) => {
-          return (
-            <div className="product" key={index}>
-              <div className="product-preview">
-                <img
-                  src={item.image}
-                  alt="some product"
-                  width="350"
-                  height="350"
-                />
-              </div>
-              <div className="product-detail">
-                <h1>Product name: {item.name}</h1>
-                <h2>Product price: {item.price}Baht</h2>
-                <p>Product description: {item.description}</p>
-              </div>
+  if (isLoading) {
+    return (
+      <h1 style={{ textAlign: 'center', fontSize: '70px', color: 'green' }}>
+        Loading...
+      </h1>
+    );
+  } else if (!isLoading && serverError) {
+    return (
+      <h1 style={{ textAlign: 'center', fontSize: '70px', color: 'crimson' }}>
+        Fetching Error...
+      </h1>
+    );
+  } else {
+    return (
+      <div className="App">
+        <div className="app-wrapper">
+          <h1 className="app-title">Products</h1>
+        </div>
+        <div className="product-list">
+          {productList.map((item, index) => {
+            return (
+              <div className="product" key={index}>
+                <div className="product-preview">
+                  <img
+                    src={item.image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {item.name}</h1>
+                  <h2>Product price: {item.price}Baht</h2>
+                  <p>Product description: {item.description}</p>
+                </div>
 
-              <button
-                className="delete-button"
-                onClick={() => handleDelete(item.id)}
-              >
-                x
-              </button>
-            </div>
-          );
-        })}
+                <button
+                  className="delete-button"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  x
+                </button>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
